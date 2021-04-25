d741fc3f6878fb695578c050b576467ac25d2728
jinyongfeng <15757127193@163.com>
2021-04-25 18:04:16 +0800

    [update]init project

---
+++ Example/FGMediaCache/FGViewController.m
@@ -0,0 +1,29 @@
+//
+//  FGViewController.m
+//  FGMediaCache
+//
+//  Created by 15757127193@163.com on 04/25/2021.
+//  Copyright (c) 2021 15757127193@163.com. All rights reserved.
+//
+
+#import "FGViewController.h"
+
+@interface FGViewController ()
+
+@end
+
+@implementation FGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
